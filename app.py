89fff1a9dd86b2ebea7b4c81fb17abb108701c52
Abed Nganga <ngangaabed@gmail.com>
2023-11-21 00:57:22 +0300

    Added error handling using HTTPException

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 import uvicorn
 import requests
 import os
@@ -12,11 +12,24 @@ APP_API_KEY = os.getenv('CONVERTER_API_KEY', None)
 # Define a route for the root endpoint
 @app.get('/')
 async def index():
-    # Make a request to the exchange rate API using the API key
-    r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
-    
-    # Return the API response as a JSON with a 200 status code
-    return Response(r.content, status_code=200, media_type='application/json')
+    if not APP_API_KEY:
+        # Raise HTTPException with a 500 status code and a detailed error message
+        raise HTTPException(
+            status_code=500,
+            detail='Server encountered an error. Please ensure that CONVERTER_API_KEY is set.'
+        )
+    else:
+        # Make a request to the exchange rate API using the API key
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
+        if r.status_code == 200:
+            # Return the API response as JSON with a 200 status code
+            return Response(r.content, status_code=200, media_type='application/json')
+        else:
+            # Raise HTTPException with a 502 status code and a detailed error message
+            raise HTTPException(
+                status_code=502,
+                detail='Server is unable to complete the request at the moment. Please try again later.'
+            )
 
 # Run the FastAPI app using Uvicorn if the script is executed directly
 if __name__ == '__main__':
