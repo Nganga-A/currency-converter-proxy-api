ced04ea7a36346f9546a2efd80f19af42785b246
Abed Nganga <ngangaabed@gmail.com>
2023-11-21 00:18:30 +0300

    Initialized fastapi and uvicorn to run the app

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+    return {"message": "Hello, World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', host='127.0.0.1', port=5555, log_level='info', reload=True)
