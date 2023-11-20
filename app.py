6b1a18047cc421609d3d45cbe34aabe498ae9f55
Abed Nganga <ngangaabed@gmail.com>
2023-11-21 01:48:43 +0300

    Finished redis configuration

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Retrieve the API key from environment variables
 APP_API_KEY = os.getenv('CONVERTER_API_KEY', None)
 
-
+# Retrieve the Redis connection URL from the environment variable 'REDIS_URL'
 REDIS_URL = os.getenv('REDIS_URL')
 redis_connection = redis.from_url(REDIS_URL)
 
@@ -21,17 +21,21 @@ Read more here: https://fastapi.tiangolo.com/tutorial/cors/
 """
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #Allow any origin to make cross-origin requests
-    allow_credentials=True, #Allow credentials
-    allow_methods=["*"], #Allow all HTTP methods
-    allow_headers=["*"], #Allow all HTTP request headers
+    allow_origins=["*"],  # Allow any origin to make cross-origin requests
+    allow_credentials=True,  # Allow credentials
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP request headers
 )
 
 # Define a route for the root endpoint
 @app.get('/')
 async def index():
+    # Check if rates are cached and return cached values if possible
+    conversion_rates = redis_connection.get('USD')
+    if conversion_rates:
+        return Response(conversion_rates, status_code=200, media_type='application/json')
+
     if not APP_API_KEY:
-        # Raise HTTPException with a 500 status code and a detailed error message
         raise HTTPException(
             status_code=500,
             detail='Server encountered an error. Please ensure that CONVERTER_API_KEY is set.'
@@ -40,18 +44,24 @@ async def index():
         # Make a request to the exchange rate API using the API key
         r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
         if r.status_code == 200:
-            # Return the API response as JSON with a 200 status code
+            # Cache newly received rates
+            redis_connection.set('USD', r.content)
+            redis_connection.expire('USD', 43200)
             return Response(r.content, status_code=200, media_type='application/json')
         else:
-            # Raise HTTPException with a 502 status code and a detailed error message
             raise HTTPException(
                 status_code=502,
                 detail='Server is unable to complete the request at the moment. Please try again later.'
             )
 
-#Define a route for the latest rates for the specified currency
+# Define a route for the latest rates for the specified currency
 @app.get('/rates/{base_currency}')
 async def get_rates(base_currency):
+    # Check if rates are cached and return cached values if possible
+    conversion_rates = redis_connection.get(base_currency)
+    if conversion_rates:
+        return Response(conversion_rates, status_code=200, media_type='application/json')
+
     if not APP_API_KEY:
         raise HTTPException(
             status_code=500,
@@ -60,6 +70,9 @@ async def get_rates(base_currency):
     else:
         r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/{base_currency}')
         if r.status_code == 200:
+            # Cache newly received rates
+            redis_connection.set(base_currency, r.content)
+            redis_connection.expire(base_currency, 43200)
             return Response(r.content, status_code=200, media_type='application/json')
         else:
             raise HTTPException(
@@ -76,10 +89,10 @@ async def convert(base_currency: str, target_currency: str, amount: float):
             status_code=500,
             detail='Server encountered an error. Please ensure that CONVERTER_API_KEY is set.'
         )
-    
+
     # Make a request to the exchange rate API using the API key
     r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/pair/{base_currency}/{target_currency}/{amount}')
-    
+
     if r.status_code == 200:
         # Return the API response as JSON with a 200 status code
         return Response(r.content, status_code=200, media_type='application/json')
@@ -96,4 +109,3 @@ if __name__ == '__main__':
 
     # Run the FastAPI app using Uvicorn
     uvicorn.run('app:app', host='0.0.0.0', port=port, log_level='info', reload=True)
-
