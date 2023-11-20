a7e41069a40909e91dcfac4d1385fd4bd281f372
Abed Nganga <ngangaabed@gmail.com>
2023-11-21 00:42:14 +0300

    Added API key to os environment variables and changed code to append url

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI,Response
 import uvicorn
 import requests
+import os 
 
 app = FastAPI()
+
+APP_API_KEY = os.getenv('CONVERTER_API_KEY', None)
 
 @app.get('/')
 async def index():
     # return {"message": "Hello, World"}
-    r = requests.get('https://v6.exchangerate-api.com/v6/9dbecabcce95c9acb31361fa/latest/USD')
+    r = requests.get(f'https://v6.exchangerate-api.com/v6/{APP_API_KEY}/latest/USD')
     return Response(r.content, status_code=200, media_type='application.json')
 
 if __name__ == '__main__':
